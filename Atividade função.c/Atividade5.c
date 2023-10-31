fb0b47a6607dcf0387629d92394fc7a5168e801f
Luiz Guilherme Oliveira da Mota <137472016+zLuizGuilhermez@users.noreply.github.com>
2023-10-30 21:38:57 -0300

    Create Atividade5.c

---
+++ Atividade função.c/Atividade5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void calculo(int mes){
+  int diasMes[12] = {31,28,30,31,30,31,30,31,30,31,30,31};
+  return diaMes[]
+}
+int main(){
+  int dia, mes, ano;
+  int diaAtual, mesAtual, anoAtual;
+
+  printf("Digite a sua data de nascimento");
+  scanf("%d %d %d", &dia &mes &ano);
+  
+  printf("Digite a sua data atual);
+  scanf("%d %d %d", &diaAtual &mesAtual &anoAtual);
+  
+}
