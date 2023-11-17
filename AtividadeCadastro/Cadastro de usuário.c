91d951082666f5161ad488885aba7deb731424eb
luiz <luizerae.s@gmail.com>
2023-11-17 15:46:00 -0300

    .\AtividadeCadastro\

--- AtividadeCadastro/Cadastro de usuário.c
+++ AtividadeCadastro/Cadastro de usuário.c
@@ -1,1 +1,19 @@
+#include<sdio.h>
+#include<string.h>
+#define MAX 1000
 
+typedef struct{
+    char nome[20];
+    char email[20];
+}
+int main(){
+    int contador = 0, opcao = 0;
+
+    do
+    printf("Opção 1 Cadastro / \n opção 2 Listar todos os clientes \n opção 3 Buscar usuario \n Opção 4 Excluir um usuario \n opção 5");
+    
+    {
+        
+    } while ();
+      
+}
