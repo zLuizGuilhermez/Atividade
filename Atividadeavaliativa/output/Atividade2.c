c6d1c66b2edf214ce3c03f0c01323853d02bc34c
luiz <luizerae.s@gmail.com>
2023-11-20 15:34:59 -0300

    .\Atividadeavaliativa\

--- Atividadeavaliativa/output/Atividade2.c
+++ Atividadeavaliativa/output/Atividade2.c
@@ -1,105 +1,107 @@
 #include<stdio.h>
-#define MAX 3
 #include<string.h>
 
-typedef struct{
-    char nome[20], sexo,qualifisa[6];
-    float salario;
-}pessoa;
-int classificaSalario(pessoa a[],int indice,float salario);
+#define MAX 1000
 
-void cadastrar(pessoa a[], int indice){
+typedef struct {
+    char nome[20], sexo, qualifisa[6];
+    float salario;
+} pessoa;
+
+void classificaSalario(pessoa a[], int indice, int *salarioMaior, int *salarioMenor);
+
+void cadastrar(pessoa a[], int indice, int *salarioMaior, int *salarioMenor){
     int escolha = 0;
-    do
-    {
+    do {
         fflush(stdin);
-        printf("\nDigite o nome do pessoa");
-        getchar();
+        printf("\nDigite o nome do pessoa: ");
         fflush(stdin);
-        fgets(a[indice].nome,sizeof(a[indice].nome), stdin);
+        fgets(a[indice].nome, sizeof(a[indice].nome), stdin);
         a[indice].nome[strcspn(a[indice].nome, "\n")] = '\0';
         fflush(stdin);
 
-        printf("Digite o seu salario");
+        printf("Digite o seu salario: ");
         scanf("%f", &a[indice].salario);
-        if (a[indice].salario <= 1)
-    {
-        printf("Deseja inserir novamente 1 s/0 n");
-        scanf("%d", &escolha);
-        if (escolha = 0)
-        {
-            return 0;
+        if (a[indice].salario <= 1) {
+            printf("Deseja inserir novamente? 1 para sim, 0 para não: ");
+            scanf("%d", &escolha);
+            if (escolha == 0) {
+                return;
+            }
         }
-    }
-    
-        printf("\nDigite o seu sexo (m ou f)");
+        classificaSalario(a, indice, salarioMaior, salarioMenor);
+
+        printf("\nDigite o seu sexo (M ou F): ");
         getchar();
-        fflush(stdin);
-    scanf(" %c", &a[indice].sexo);
-    if (a[indice].sexo != 'm' || a[indice].sexo != 'f')
-    {
-        printf("Deseja inserir novamente 1 s/0 n");
-        scanf("%d", &escolha);
-        if (escolha = 0)
-        {
-            return 0;
+        fflush(stdin);  
+        scanf(" %c", &a[indice].sexo);
+        if (a[indice].sexo != 'M' && a[indice].sexo != 'F') {
+            printf("Deseja inserir novamente? 1 para sim, 0 para não: ");
+            scanf("%d", &escolha);
+            if (escolha == 0) {
+                return;
+            }
         }
-        
-    }
-    
-} while (escolha != 0);
-}
-int classificaSalario(pessoa a[],int indice,float salario){
 
-    if (a[indice].salario < 1400){
-    strcpy(a[indice].qualifisa, "Abaixo");
-    }else if (a[indice].salario == 1400){
-    strcpy(a[indice].qualifisa, "Igual");
-    
-    }else{
-    strcpy(a[indice].qualifisa, "Acima");
-    }
 
+    } while (escolha != 0);
 }
-    
-void mostraClassifica(pessoa a[], int indice){
-    if(indice == 0){
-        printf("\nnenhum dado cadastrado");
-        
-    }else{
-        for(int i = 0; i < indice; i++){
-            printf("\nNome %s", a[i].nome); 
-            printf("\nSexo %c", a[i].sexo);
-            printf("\n Salario %0.2f", a[i].salario);
-            printf("Situação salario %s", a[i].qualifisa);
+
+void classificaSalario(pessoa a[], int indice, int *salarioMaior, int *salarioMenor) {
+    if (a[indice].salario < 1400) {
+        strcpy(a[indice].qualifisa, "Abaixo");
+        (*salarioMenor)++;
+    } else if (a[indice].salario == 1400) {
+        strcpy(a[indice].qualifisa, "Igual");
+
+    } else {
+        strcpy(a[indice].qualifisa, "Acima");
+        (*salarioMaior)++;
+    }
+}
+
+void mostraClassifica(pessoa a[], int indice) {
+    if (indice == 0) {
+        printf("\nNenhum dado cadastrado");
+
+    } else {
+        for (int i = 0; i < indice; i++) {
+            printf("\n\nNome: %s", a[i].nome);
+            printf("\nSexo: %c", a[i].sexo);
+            printf("\nSalario: BRL %0.2f", a[i].salario);
+            printf("\nSituacao salario: %s", a[i].qualifisa);
         }
     }
 }
-int main(){
-        pessoa a[MAX];
-        int indice = 0,opcao = 0,busca = 0;
-        char decisao;
-        
-        do{
-            printf("\nDigite 1 para registrar\nDigite 2 para listar\nDigite 0 para sair");
-            scanf("%d", &opcao);
-            switch(opcao){
-                case 1:
-                if(indice < MAX){
-                    cadastrar(a, indice);
+
+int main() {
+    pessoa a[MAX];
+    int indice = 0, opcao = 0, salarioMaior = 0, salarioMenor = 0;
+
+    do {
+        printf("\n\nDigite 1 para registrar\nDigite 2 para listar\n Digite 3 para mostrar as estatisticas dos salarios \nDigite 0 para sair: ");
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case 1:
+                if (indice < MAX) {
+                    cadastrar(a, indice, &salarioMaior, &salarioMenor);
                     indice++;
                     printf("\nCadastro realizado com sucesso");
-                    
-                }else{
+
+                } else {
                     printf("\nCapacidade maxima atingida");
                 }
                 break;
-                case 2:
+            case 2:
                     mostraClassifica(a, indice);
-                break;
-               
-            }
-            
-        }while(opcao != 0);
-        return 0;
+                    break;
+            case 3:
+                    
+                    printf("\nQuantidade de pessoas com salarios menores:%d", salarioMenor);
+                    printf("\nQuantidade de pessoas com salarios maiores:%d", salarioMaior);
+
+        }
+
+    } while (opcao != 0);
+    return 0;
 }
